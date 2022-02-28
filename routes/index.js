6e6fec229d5b6284636dd81c49adc0b0ad778c0c
levanagautier <levana.gautier@gmail.com>
2022-02-28 17:05:43 +0100

    Créations des Views + dossier Public. Début de configuration des Views et du MainRouter

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,19 @@
-import { Router as router } from "express";
+import express from 'express';
 
-export default router;
+const MainRouter = express.Router();
+
+MainRouter.get('/', (req, res, next) => {
+    res.render('main/homepage', {});
+    return;
+
+})
+
+MainRouter.get('*', (req, res, next) => {
+    res.render('main/error', {
+        error: '404 — La page que vous avez demandé n\'existe pas'
+    });
+    return;
+
+})
+
+export default MainRouter;
